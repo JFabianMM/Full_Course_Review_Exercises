8a2f6860d49ddbcaaccf2b15c6e41c015eee2a2e
JFabianMM <103282271+JFabianMM@users.noreply.github.com>
2023-09-11 14:33:05 -0700

    Add files via upload

--- Exercise-03/exercise03.js
+++ Exercise-03/exercise03.js
@@ -1,15 +1,20 @@
-require('isomorphic-fetch');
+class CancelableFetch {
+    constructor (url){
+        this.controller = new AbortController();
+        this.promise=fetch(url, {   
+                        signal:this.controller.signal,
+                    })
+    }
+    cancel(){
+        this.controller.abort();
+    }
+    then(callback){
+        return this.promise.then(callback)
+    }
+    catch(callback){
+        return this.promise.catch(callback)
+    }
+} 
 
-let cancellableFetch = async function (url) {
-    controller = new AbortController();
-    let res=  fetch(url, {   
-        signal: controller.signal,                   
-    }).then((result) => {
-        return result.json();
-    }).catch(error => {
-        return error.name;
-    });
-    return res;
-    };
-
-module.exports = cancellableFetch
+const cancellableFetch = (url) => new CancelableFetch(url); 
+module.exports = cancellableFetch;
--- Exercise-03/exercise03.test.js
+++ Exercise-03/exercise03.test.js
@@ -1,17 +1,17 @@
-const cancellableFetch1 = require('./exercise03.js')
+const cancellableFetch = require('./exercise03.js')
 
-test('Url answear after 3 seconds and aborted after 4 seconds (No aborted). Result="OK', (done) => {
+test("Fetch(url) answear is not aborted. Result should be={count:40372,name:'bella',age:42}", (done) => {
     const url='https://api.agify.io/?name=bella';
-    let result = cancellableFetch1(url);
-    result.then(result=>expect(result).toMatchObject({count:40372,name:'bella',age:42}));
-      done()
+    let result = cancellableFetch(url);
+    result.then((result) => {return result.json();}).then(result=>expect(result).toMatchObject({count:40372,name:'bella',age:42}));
+    done();
   });
 
-test('Url answear after 3 seconds and aborted after 2 seconds (Aborted). Result="AbortError"', (done) => {
+test('Fetch(url) answear should be received after 3 seconds, but is aborted after 2 seconds. Result="AbortError"', (done) => {
   const url='https://hub.dummyapis.com/delay?seconds=3';
-  let result = cancellableFetch1(url);
-  result.cancel=function(){controller.abort()};
+  let result = cancellableFetch(url, true);
   setTimeout(() => result.cancel(), 2000); 
-  result.then(result=>expect(result).toBe('AbortError'));
-    done()
+  result.then((result) => {return result.json();}).catch(error => {return error.name;}).then(result=>expect(result).toBe('AbortError'));
+  done();
 });
+
